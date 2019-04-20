be7d9d87e8379d57a20bf730b7e2748cc6f0e43c
mamenesia <dedy.prasetyo.h@gmail.com>
2019-04-20 13:23:03 +0700

    adding bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  for(var i = arr.length; i > 0; i--) {
+    for(var j = 0; j < i; j++) {
+      if(arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+bubbleSort([5, 3, 8, 2, 1, 4]);
+bubbleSort([20, 20, 31, 56, 1, 12, 12]);
