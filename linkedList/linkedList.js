3b107e4705dfe8932d2e5f838f53b2972dce5278
mamenesia <dedy.prasetyo.h@gmail.com>
2019-05-12 05:06:17 +0700

    adding node implementation

---
+++ linkedList/linkedList.js
@@ -0,0 +1,10 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
