4e59e811f1d3bfeda05b2a8fd6f69d2fe3bb0436
mamenesia <dedy.prasetyo.h@gmail.com>
2019-05-16 21:46:06 +0700

    adding getFirst, getLast and clear method to linkedList

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -27,5 +27,28 @@ class LinkedList {
     }
     return counter;
   }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if(!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while(node) {
+      if(!node.next) {
+        return node;
+      }
+
+      node = node.next;
+    }
+
+    clear() {
+      this.head = null;
+    }
+  }
 }
 
