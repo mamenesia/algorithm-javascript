eea8c32622b840ee9c72650945d2486072bc817a
mamenesia <dedy.prasetyo.h@gmail.com>
2019-05-21 22:23:21 +0700

    refactoring methods using available method

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -15,7 +15,8 @@ class LinkedList {
   }
 
   insertFirst(data) {
-    this.head = new Node(data, this.head);
+    // this.head = new Node(data, this.head);
+    this.insertAt(data,0);
   }
 
   size() {
@@ -29,21 +30,24 @@ class LinkedList {
   }
 
   getFirst() {
-    return this.head;
+    // return this.head;
+    return this.getAt(0);
   }
 
   getLast() {
-    if(!this.head) {
-      return null;
-    }
+    // if(!this.head) {
+    //   return null;
+    // }
 
-    let node = this.head;
-    while(node) {
-      if(!node.next) {
-        return node;
-      }
-      node = node.next;
-    }
+    // let node = this.head;
+    // while(node) {
+    //   if(!node.next) {
+    //     return node;
+    //   }
+    //   node = node.next;
+    // }
+
+    return this.getAt(this.size()-1);
   }
 
   clear() {
@@ -51,39 +55,43 @@ class LinkedList {
   }
 
   removeFirst() {
-    if(!this.head) {
-      return;
-    }
+    // if(!this.head) {
+    //   return;
+    // }
 
-    this.head = this.head.next;
+    // this.head = this.head.next;
+    this.removeAt(0);
   }
 
   removeLast() {
     
-    if(!this.head) {
-      return;
-    } else if(!this.head.next) {
-      previous = null;
-      return;
-    }
+    // if(!this.head) {
+    //   return;
+    // } else if(!this.head.next) {
+    //   previous = null;
+    //   return;
+    // }
 
-    let previous = this.head;
-    let node = this.head.next;
+    // let previous = this.head;
+    // let node = this.head.next;
 
-    while(node.next) {
-      previous = node;
-      node = node.next;
-    }
+    // while(node.next) {
+    //   previous = node;
+    //   node = node.next;
+    // }
 
-    previous.next = null;
+    // previous.next = null;
+
+    this.removeAt(this.size()-1);
   }
 
   insertLast(data) {
-    if(!this.head) {
-      this.head = new Node(data);
-    } else {
-    this.getLast().next = new Node(data);
-    }
+    // if(!this.head) {
+    //   this.head = new Node(data);
+    // } else {
+    // this.getLast().next = new Node(data);
+    // }
+    this.insertAt(data, this.size()-1);
   }
 
   getAt(index) {
